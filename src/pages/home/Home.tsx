8f9ffb06afc70584ce27e811d5557c8b1c018afb
GasparLeonardi <gasparlleonardi@gmail.com>
2023-10-23 13:50:45 -0300

    BlogPessoal Front

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,25 @@
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <div className='pagina'>
+                <div>
+                    <div className='titulo'>
+                        <h2>Seja Bem Vinde!</h2>
+                        <p>Expresse aqui seus pensamentos e opniões</p>
+                    </div>
+
+                    <div className='img'>
+                        <img 
+                            src="https://i.imgur.com/VpwApCU.png" 
+                            alt="Imagem da Página Home" 
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
