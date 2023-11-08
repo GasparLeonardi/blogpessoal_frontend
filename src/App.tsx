cb0d531dd6a45c41618d2cd4192f768cd5e1f931
GasparLeonardi <gasparlleonardi@gmail.com>
2023-11-08 15:57:35 -0300

    Detalhes Pre Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Perfil from './pages/perfil/Perfil';
+
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Footer from './components/footer/Footer';
@@ -17,6 +21,7 @@ import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPosta
 function App() {
     return (
         <AuthProvider>
+            <ToastContainer />
             <BrowserRouter>
                 <Navbar />
                 <div className='min-h-[80vh]'>
@@ -33,6 +38,7 @@ function App() {
                         <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
                         <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
                         <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+                        <Route path="/perfil" element={<Perfil />} />
                     </Routes>
                 </div>
                 <Footer />
---
+++ src/utils/toastAlerta.ts
@@ -0,0 +1,46 @@
+import { toast } from "react-toastify";
+
+export function toastAlerta(mensagem: string, tipo: string) {
+  switch (tipo) {
+
+    case "sucesso":
+        toast.success(mensagem, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+      break;
+
+    case "info":
+        toast.info(mensagem, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+        break;
+
+    case "erro":
+        toast.error(mensagem, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+        break;
+
+  }
+}
